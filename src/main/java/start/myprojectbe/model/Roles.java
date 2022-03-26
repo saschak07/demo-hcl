4d45bedae0ec73797d35ae8c6bdce37c56d3bb3c
tnguyen <thiminh.nguyen@bertelsmann.de>
2022-03-26 14:28:46 +0800

    [Init Project] - Add role

---
+++ src/main/java/start/myprojectbe/model/Roles.java
@@ -0,0 +1,10 @@
+package start.myprojectbe.model;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Roles {
+	public static final String ADMIN_ROLE = "ADMIN";
+	public static final String USER_ROLE = "USER";
+}
